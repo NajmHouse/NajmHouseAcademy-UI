f5a2054b2bbe453aa63ce53fa7a9d3508fada966
sharique <sharique.kalim.1994@gmail.com>
2024-10-07 22:49:47 +0530

    Initial Commit for UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component'
+import { FooterComponent } from './shared/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,FooterComponent,LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
